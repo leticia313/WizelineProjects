1f6cbc2f24df778863efba1492585730a4d355bf
leticia313 <mtstisha@gmail.com>
2023-05-29 15:06:31 -0600

    sube gitignore

---
+++ Ejercicios/TiposDatos.py
@@ -0,0 +1,10 @@
+
+valor1 = int(2)
+valor2 = float(2.5)
+valor3 = str("lety")
+valor4 = bool("false")
+
+print(type(valor1))
+print(type(valor2))
+print(type(valor3))
+print(type(valor4))
---
+++ Ejercicios/list.py
@@ -0,0 +1,13 @@
+numeros = [15,10,1,0,-1,99,12,17,2,11,37,-99]
+max_numero = max(numeros)
+print('mi número máximo es :', max_numero)
+min_numero = min(numeros)
+print('mi número minimo es:', min_numero)
+print('mi primera mitad es:', numeros[:6])
+print('mi segunda mitad es:', numeros[6:])
+print(sum(numeros[:6]))
+print(sum(numeros[6:]))
+slista = numeros[:6]
+print('el segundo número de mi lista es:', slista[1])
+sslista = numeros[6:]
+print('el segundo número desde atras de la segunda lista es:', sslista[-2])
